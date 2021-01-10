df546371c1ecf303bf6f30d49399265b6156b698
Abhishek Kumar <iabhishek5007@gmail.com>
2021-01-10 21:17:50 +0530

    Initial commit

---
+++ app/src/main/java/com/rckdeveloper/kanchenjungalotterries/HomeActivity.java
@@ -0,0 +1,178 @@
+package com.rckdeveloper.kanchenjungalotterries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import im.delight.android.webview.AdvancedWebView;
+
+public class HomeActivity extends AppCompatActivity implements AdvancedWebView.Listener{
+
+    private static final String TEST_PAGE_URL = "http://kanchenjungalotterries.com/";
+    private AdvancedWebView mWebView;
+    boolean doubleBackToExitPressedOnce = false;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        // progressDialog.show();
+
+        mWebView = (AdvancedWebView) findViewById(R.id.webview);
+        mWebView.setListener(this, this);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setGeolocationEnabled(true);
+        mWebView.setMixedContentAllowed(true);
+        mWebView.setCookiesEnabled(true);
+        mWebView.setThirdPartyCookiesEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+
+
+        });
+        mWebView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                //   Toast.makeText(HomePage.this, title, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        mWebView.addHttpHeader("X-Requested-With", "");
+        mWebView.loadUrl(TEST_PAGE_URL);
+
+
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWebView.onResume();
+        // ...
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onPause() {
+        mWebView.onPause();
+        // ...
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mWebView.onDestroy();
+        // ...
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        mWebView.onActivityResult(requestCode, resultCode, intent);
+        // ...
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!mWebView.onBackPressed()) { return; }
+        // ...
+
+        if (doubleBackToExitPressedOnce) {
+            // Log.i(TAG, "double click");
+            new AlertDialog.Builder(this)
+                    .setIcon(R.drawable.kanchenjungalotterries)
+                    .setTitle("Press Exit To Close App:")
+                    .setMessage("Are you sure to exit?")
+                    .setPositiveButton("Exit",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog,
+                                                    int which) {
+                                    finish();
+                                }
+
+                            }).setNegativeButton("No", null).show();
+            return;
+        } else {
+            //   Log.i(TAG, "single click");
+            if (mWebView.canGoBack()) {
+                //   Log.i(TAG, "canGoBack");
+                mWebView.goBack();
+            } else {
+                //   Log.i(TAG, "nothing to canGoBack");
+            }
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        if (getApplicationContext() == null) {
+            return;
+        } else {
+            //    Toast.makeText(this, "Please click BACK again to exit",
+            //     Toast.LENGTH_SHORT).show();
+        }
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+//        super.onBackPressed();
+    }
+
+
+    @Override
+    public void onPageStarted(String url, Bitmap favicon) {
+        mWebView.setVisibility(View.INVISIBLE);
+        progressDialog.show();
+    }
+
+    @Override
+    public void onPageFinished(String url) {
+        mWebView.setVisibility(View.VISIBLE);
+        progressDialog.dismiss();
+    }
+
+    @Override
+    public void onPageError(int errorCode, String description, String failingUrl) {
+        Toast.makeText(HomeActivity.this, "onPageError(errorCode = "+errorCode+",  description = "+description+",  failingUrl = "+failingUrl+")", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
+       // Toast.makeText(HomeActivity.this, "onDownloadRequested(url = "+url+",  suggestedFilename = "+suggestedFilename+",  mimeType = "+mimeType+",  contentLength = "+contentLength+",  contentDisposition = "+contentDisposition+",  userAgent = "+userAgent+")", Toast.LENGTH_LONG).show();
+        mWebView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url="+url);
+//        if (AdvancedWebView.handleDownload(this, url, suggestedFilename)) {
+//            // download successfully handled
+//        }
+//        else {
+//            // download couldn't be handled because user has disabled download manager app on the device
+//            // TODO show some notice to the user
+//        }
+    }
+
+    @Override
+    public void onExternalPageRequest(String url) {
+        Toast.makeText(HomeActivity.this, "onExternalPageRequest(url = "+url+")", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/rckdeveloper/kanchenjungalotterries/MainActivity.java
@@ -0,0 +1,36 @@
+package com.rckdeveloper.kanchenjungalotterries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
